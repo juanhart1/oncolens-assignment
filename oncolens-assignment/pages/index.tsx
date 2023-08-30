ec0002e7a4701fbc9b4e007e43ef723c3dd3f7ad
Juan Hart <juanhart1@gmail.com>
2023-08-30 09:56:33 -0500

    Implementation of list items being selectable and having an annotation that it's each list item is selected

--- oncolens-assignment/pages/index.tsx
+++ oncolens-assignment/pages/index.tsx
@@ -1,50 +1,85 @@
 import { useReducer, useState } from "react";
+import clsx from "clsx";
 
 // constants
-const selected = [
-  { label: "Apple", identifier: "Apple1" },
-  { label: "Grape", identifier: "Grape1" },
-];
-const notSelected = [
-  { label: "bananas", identifier: "Bananas1" },
-  { label: "blueberries", identifier: "Blueberries1" },
-  { label: "melons", identifier: "Melons1" },
+const items = [
+  { label: "bananas", identifier: "Bananas1", added: true, selected: false },
+  { label: "pears", identifier: "Pear1", added: false, selected: false },
+  { label: "melons", identifier: "Melons1", added: false, selected: false },
+  { label: "apple", identifier: "Apple1", added: false, selected: true },
+  { label: "grape", identifier: "Grape1", added: true, selected: false },
 ];
 
 // types
 type Item = {
+  added: boolean;
+  identifier: string;
   label: string;
-  identifier?: string;
+  selected?: boolean;
 };
-type State = { selectedItems: Item[]; nonSelectedItems: Item[] };
+type Items = Item[];
+type State = { addedItems: Items; allItems: Items; nonAddedItems: Items };
 type Action = { type: string; payload: string };
 
 // initial state & reducer logic
 const initialState: State = {
-  selectedItems: selected,
-  nonSelectedItems: notSelected,
+  addedItems: items.filter((item) => item.added),
+  allItems: items,
+  nonAddedItems: items.filter((item) => !item.added),
 };
 const reducer = (state: State, action: Action) => {
   switch (action.type) {
     case "Select Item": {
-      console.log("Item has been selected...");
-      return {
-        nonSelectedItems: [...state.nonSelectedItems],
-        selectedItems: [...state.selectedItems],
-      };
+      // find the matching item
+      const [selectedItem] = state.allItems.filter(
+        (item) => item.identifier === action.payload,
+      );
+
+      // ask whether selected item is added or not
+      if (selectedItem.added) {
+        // iterate over added state
+        const mappedAddedItems = state.addedItems.map((addedItem) => {
+          if (selectedItem.identifier === addedItem.identifier) {
+            return { ...addedItem, selected: !addedItem.selected };
+          }
+          return addedItem;
+        });
+
+        return {
+          addedItems: mappedAddedItems,
+          allItems: state.allItems,
+          nonAddedItems: [...state.nonAddedItems],
+        };
+      } else {
+        // iterate over not added state
+        const mappedNotAddedItems = state.nonAddedItems.map((notAddedItem) => {
+          if (selectedItem.identifier === notAddedItem.identifier) {
+            return { ...notAddedItem, selected: !notAddedItem.selected };
+          }
+          return notAddedItem;
+        });
+
+        return {
+          addedItems: [...state.addedItems],
+          allItems: state.allItems,
+          nonAddedItems: mappedNotAddedItems,
+        };
+      }
     }
     case "Add Item": {
       console.log("Item would be added...");
       return {
-        nonSelectedItems: [...state.nonSelectedItems],
-        selectedItems: [...state.selectedItems],
+        addedItems: [...state.addedItems],
+        allItems: [...state.allItems],
+        nonAddedItems: [...state.nonAddedItems],
       };
     }
     case "Remove Item": {
       console.log("Item would be removed...");
       return {
-        nonSelectedItems: [...state.nonSelectedItems],
-        selectedItems: [...state.selectedItems],
+        addedItems: [...state.addedItems],
+        allItems: [...state.allItems],
+        nonAddedItems: [...state.nonAddedItems],
       };
     }
     default:
@@ -61,10 +96,10 @@ const IndexPage = () => {
   const [nonSelectedItemsValue, setnonSelectedItemsValue] = useState("");
 
   // filtered items based on their corresponding input
-  const filteredSelectedItems = items.selectedItems.filter((item) =>
+  const filteredSelectedItems = items.addedItems.filter((item) =>
     item.label.toLowerCase().startsWith(selectedItemsValue),
   );
-  const filteredNonSelectedItems = items.nonSelectedItems.filter((item) =>
+  const filteredNonSelectedItems = items.nonAddedItems.filter((item) =>
     item.label.toLowerCase().startsWith(nonSelectedItemsValue),
   );
 
@@ -75,9 +110,11 @@ const IndexPage = () => {
         <ul>
           {filteredNonSelectedItems.map((item) => (
             <li
-              className="capitalize"
+              className={clsx("capitalize", item.selected ? "border" : "")}
               key={item.identifier}
-              onClick={() => console.log({ identifier: item.identifier })}
+              onClick={() => {
+                dispatch({ payload: item.identifier, type: "Select Item" });
+              }}
             >
               {item.label}
             </li>
@@ -94,9 +131,11 @@ const IndexPage = () => {
         <ul>
           {filteredSelectedItems.map((item) => (
             <li
-              className="capitalize"
+              className={clsx("capitalize", item.selected ? "border" : "")}
               key={item.identifier}
-              onClick={() => console.log({ identifier: item.identifier })}
+              onClick={() => {
+                dispatch({ payload: item.identifier, type: "Select Item" });
+              }}
             >
               {item.label}
             </li>
