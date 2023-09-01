31e41cbfb826fb59909ef04483ba58babcf548d6
Juan Hart <juanhart1@gmail.com>
2023-09-01 10:38:17 -0500

    Tweaked styling to make more visually pleasing

--- components/ListHeader.tsx
+++ components/ListHeader.tsx
@@ -2,7 +2,7 @@ import { ListHeaderProps } from "../interfaces";
 
 const ListHeader = ({ title }: ListHeaderProps) => (
   <header className="text-center my-4">
-    <h1 className="text-center underline decoration-wavy">{title}</h1>
+    <h1 className="text-center underline underline-offset-8 decoration-wavy">{title}</h1>
   </header>
 );
 
